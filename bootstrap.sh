c05a0e50eddfc4425b6baa79943baa9b0d1d2197
EastArctica <eastarctica@gmail.com>
2024-03-08 16:02:15 -0500

    Add gtk-3.0 settings

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,4 +5,5 @@ ln -s "$PWD/.gitconfig" "$HOME/"
 ln -s "$PWD/hypr/" "$HOME/.config/"
 ln -s "$PWD/waybar/" "$HOME/.config/"
 ln -s "$PWD/pipes-rs/" "$HOME/.config/"
+ln -s "$PWD/gtk-3.0/" "$HOME/.config/"
 
