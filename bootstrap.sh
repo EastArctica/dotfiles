607a21f73b23b6c4c4619964fddc506085cd3bc2
EastArctica <eastarctica@gmail.com>
2024-03-08 17:35:48 -0500

    Update bootstrapping system

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,47 @@
 #!/bin/bash
 
-ln -s "$PWD/.gitconfig" "$HOME/"
-ln -s "$PWD/.gtkrc-2.0" "$HOME/"
+link() {
+	# Source is $1, Target is $2
+	if [ -f "$1" ]; then
+		# Source is a file
+		local target_path="$2/$(basename $1)"
+		if [ -L "$target_path" ] && [ "$(readlink -f "$target_path")" = "$(realpath "$1")" ]; then
+			echo "$target_path already linked"
+		elif [ -L "$target_path" ]; then
+			echo "Failed to link $target_path, already points to $(readlink -f "$target_path")"
+		elif [ -e "$target_path" ]; then
+			echo "Failed to link $target_path, a file already exists"
+		else
+			echo "Linking file $1 to $target_path"
+			ln -s "$1" "$target_path"
+		fi
+	elif [ -d "$1" ]; then
+		# Source is a directory
+		local target_path="$2/$(basename $1)"
+		if [ -L "$target_path" ] && [ "$(readlink -f "$target_path")" = "$(realpath "$1")" ]; then
+			echo "$target_path already linked"
+		elif [ -L "$target_path" ]; then
+			echo "Failed to link $target_path, already points to $(readlink -f "$target_path")"
+		elif [ -e "$target_path" ]; then
+			echo "Failed to link $target_path, a directory already exists"
+		else
+			echo "Linking directory $1 to $target_path"
+			ln -s "$1" "$target_path"
+		fi
+	else
+		echo "Source $1 does not exist"
+	fi
+}
 
-ln -s "$PWD/hypr/" "$HOME/.config/"
-ln -s "$PWD/waybar/" "$HOME/.config/"
-ln -s "$PWD/pipes-rs/" "$HOME/.config/"
-ln -s "$PWD/gtk-3.0/" "$HOME/.config/"
-ln -s "$PWD/cava/" "$HOME/.config/"
-ln -s "$PWD/kitty/" "$HOME/.config/"
-ln -s "$PWD/wofi/" "$HOME/.config"
+
+link "$PWD/.gitconfig" "$HOME"
+link "$PWD/.gtkrc-2.0" "$HOME"
+
+link "$PWD/hypr/" "$HOME/.config"
+link "$PWD/waybar/" "$HOME/.config"
+link "$PWD/pipes-rs/" "$HOME/.config"
+link "$PWD/gtk-3.0/" "$HOME/.config"
+link "$PWD/cava/" "$HOME/.config"
+link "$PWD/kitty/" "$HOME/.config"
+link "$PWD/wofi/" "$HOME/.config"
 
