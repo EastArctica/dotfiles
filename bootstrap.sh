328db59f99304a12c7091445d0e7347c02b5ac65
EastArctica <eastarctica@gmail.com>
2024-03-08 16:58:04 -0500

    Add kitty config

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,4 +8,5 @@ ln -s "$PWD/waybar/" "$HOME/.config/"
 ln -s "$PWD/pipes-rs/" "$HOME/.config/"
 ln -s "$PWD/gtk-3.0/" "$HOME/.config/"
 ln -s "$PWD/cava/" "$HOME/.config/"
+ln -s "$PWD/kitty/" "$HOME/.config/"
 
