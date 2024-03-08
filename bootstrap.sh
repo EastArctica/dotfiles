7a6316b99e53903fa184fd7798301410b41f35be
EastArctica <eastarctica@gmail.com>
2024-03-08 17:47:53 -0500

    Update bootstrapper system

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,34 +2,17 @@
 
 link() {
 	# Source is $1, Target is $2
-	if [ -f "$1" ]; then
-		# Source is a file
-		local target_path="$2/$(basename $1)"
-		if [ -L "$target_path" ] && [ "$(readlink -f "$target_path")" = "$(realpath "$1")" ]; then
-			echo "$target_path already linked"
-		elif [ -L "$target_path" ]; then
-			echo "Failed to link $target_path, already points to $(readlink -f "$target_path")"
-		elif [ -e "$target_path" ]; then
-			echo "Failed to link $target_path, a file already exists"
-		else
-			echo "Linking file $1 to $target_path"
-			ln -s "$1" "$target_path"
-		fi
-	elif [ -d "$1" ]; then
-		# Source is a directory
-		local target_path="$2/$(basename $1)"
-		if [ -L "$target_path" ] && [ "$(readlink -f "$target_path")" = "$(realpath "$1")" ]; then
-			echo "$target_path already linked"
-		elif [ -L "$target_path" ]; then
-			echo "Failed to link $target_path, already points to $(readlink -f "$target_path")"
-		elif [ -e "$target_path" ]; then
-			echo "Failed to link $target_path, a directory already exists"
-		else
-			echo "Linking directory $1 to $target_path"
-			ln -s "$1" "$target_path"
-		fi
+	# Source is a directory
+	local target_path="$2/$(basename $1)"
+	if [ -L "$target_path" ] && [ "$(readlink -f "$target_path")" = "$(realpath "$1")" ]; then
+		echo "$target_path already linked"
+	elif [ -L "$target_path" ]; then
+		echo "Failed to link $target_path, already points to $(readlink -f "$target_path")"
+	elif [ -e "$target_path" ]; then
+		echo "Failed to link $target_path, already exists as a non-symlink"
 	else
-		echo "Source $1 does not exist"
+		echo "Linking $1 to $target_path"
+		ln -s "$1" "$target_path"
 	fi
 }
 
